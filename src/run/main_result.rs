79eb2cc41bca7358addc742b6ed2f2923fcc58e1
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-09-29 21:18:19 +0200

    Restore original exit codes
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- src/run/main_result.rs
+++ src/run/main_result.rs
@@ -15,24 +15,28 @@ impl Termination for MainResult {
             MainResult::Good => ExitCode::from(0),
             MainResult::NoDeviceNameSupplied => {
                 eprintln!("No device name supplied");
-                ExitCode::from(1)
+                ExitCode::from(wrap_to_u8(-1))
             }
             MainResult::UAPIListenerCreationFailed(e) => {
                 eprintln!("Failed to create UAPI listener: {}", e);
-                ExitCode::from(2)
+                ExitCode::from(wrap_to_u8(-2))
             }
             MainResult::TUNDeviceCreationFailed(e) => {
                 eprintln!("Failed to create TUN device: {}", e);
-                ExitCode::from(3)
+                ExitCode::from(wrap_to_u8(-3))
             }
             MainResult::DropPriviligesFailed(e) => {
                 eprintln!("Failed to drop privileges: {}", e);
-                ExitCode::from(4)
+                ExitCode::from(wrap_to_u8(-4))
             }
             MainResult::DaemonizeFailed(e) => {
                 eprintln!("Failed to daemonize: {}", e);
-                ExitCode::from(5)
+                ExitCode::from(wrap_to_u8(-5))
             }
         }
     }
+}
+
+fn wrap_to_u8(value: i8) -> u8 {
+    0u8.wrapping_add_signed(value)
 }
