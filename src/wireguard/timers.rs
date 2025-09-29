94be9b8744edbea4f1196524ce2359928c4c82fd
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-09-29 12:19:11 +0200

    Refactor timer creation in new
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- src/wireguard/timers.rs
+++ src/wireguard/timers.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 
 use log::debug;
 
-use hjul::Timer;
+use hjul::{Runner, Timer};
 use x25519_dalek::PublicKey;
 
 use super::WireGuard;
@@ -27,6 +27,27 @@ pub struct Timers {
     new_handshake: Timer,
 }
 
+macro_rules! fetch_peer {
+    ( $wireguard_device:expr_2021, $public_key_of_peer:expr_2021, $peer:ident) => {
+        let peers = $wireguard_device.peers.read();
+        let $peer = match peers.get(&$public_key_of_peer) {
+            None => {
+                return;
+            }
+            Some(peer) => peer,
+        };
+    };
+}
+
+macro_rules! fetch_timers {
+    ( $peer:ident, $timers:ident) => {
+        let $timers = $peer.timers();
+        if !$timers.enabled {
+            return;
+        }
+    };
+}
+
 impl Timers {
     #[inline(always)]
     fn needs_another_keepalive(&self) -> bool {
@@ -38,27 +59,6 @@ impl Timers {
         public_key_of_peer: PublicKey,
         timers_started: bool,
     ) -> Timers {
-        macro_rules! fetch_peer {
-            ( $wg:expr_2021, $pk:expr_2021, $peer:ident) => {
-                let peers = $wg.peers.read();
-                let $peer = match peers.get(&$pk) {
-                    None => {
-                        return;
-                    }
-                    Some(peer) => peer,
-                };
-            };
-        }
-
-        macro_rules! fetch_timers {
-            ( $peer:ident, $timers:ident) => {
-                let $timers = $peer.timers();
-                if !$timers.enabled {
-                    return;
-                }
-            };
-        }
-
         let runner = wireguard_device.runner.lock();
 
         // create a timer instance for the provided peer
@@ -68,99 +68,144 @@ impl Timers {
             need_another_keepalive: AtomicBool::new(false),
             sent_lastminute_handshake: AtomicBool::new(false),
             handshake_attempts: AtomicUsize::new(0),
-            retransmit_handshake: {
-                let wg = wireguard_device.clone();
-                runner.timer(move || {
-                    // fetch peer by public key
-                    fetch_peer!(wg, public_key_of_peer, peer);
-                    fetch_timers!(peer, timers);
-
-                    // check if handshake attempts remaining
-                    let attempts = timers.handshake_attempts.fetch_add(1, Ordering::SeqCst);
-                    if attempts > MAX_TIMER_HANDSHAKES {
-                        debug!(
-                            "Handshake for peer {} did not complete after {} attempts, giving up",
-                            peer,
-                            attempts + 1
-                        );
-                        timers.send_keepalive.stop();
-                        timers.zero_key_material.start(REJECT_AFTER_TIME * 3);
-                        peer.purge_staged_packets();
-                    } else {
-                        debug!(
-                            "Handshake for {} did not complete after {} seconds, retrying (try {})",
-                            peer,
-                            REKEY_TIMEOUT.as_secs(),
-                            attempts
-                        );
-                        timers.retransmit_handshake.reset(REKEY_TIMEOUT);
-                        peer.clear_src();
-                        peer.packet_send_queued_handshake_initiation(true);
-                    }
-                })
-            },
-            send_keepalive: {
-                let wg = wireguard_device.clone();
-                runner.timer(move || {
-                    // fetch peer by public key
-                    fetch_peer!(wg, public_key_of_peer, peer);
-                    fetch_timers!(peer, timers);
-
-                    // send keepalive and schedule next keepalive
-                    peer.send_keepalive();
-                    if timers.needs_another_keepalive() {
-                        timers.send_keepalive.start(KEEPALIVE_TIMEOUT);
-                    }
-                })
-            },
-            new_handshake: {
-                let wg = wireguard_device.clone();
-                runner.timer(move || {
-                    // fetch peer by public key
-                    fetch_peer!(wg, public_key_of_peer, peer);
-                    fetch_timers!(peer, timers);
-
-                    // clear source and retry
-                    log::debug!(
-                        "Retrying handshake with {} because we stopped hearing back after {} seconds",
-                        peer,
-                        (KEEPALIVE_TIMEOUT + REKEY_TIMEOUT).as_secs()
-                    );
-                    peer.clear_src();
-                    peer.packet_send_queued_handshake_initiation(false);
-                })
-            },
-            zero_key_material: {
-                let wg = wireguard_device.clone();
-                runner.timer(move || {
-                    // fetch peer by public key
-                    fetch_peer!(wg, public_key_of_peer, peer);
-                    log::trace!("{} : timer fired (zero_key_material)", peer);
-
-                    // null all key-material
-                    peer.zero_keys();
-                })
-            },
-            send_persistent_keepalive: {
-                let wg = wireguard_device.clone();
-                runner.timer(move || {
-                    // fetch peer by public key
-                    fetch_peer!(wg, public_key_of_peer, peer);
-                    fetch_timers!(peer, timers);
-                    log::trace!("{} : timer fired (send_persistent_keepalive)", peer);
-
-                    // send and schedule persistent keepalive
-                    if timers.keepalive_interval > 0 {
-                        timers.send_keepalive.stop();
-                        peer.send_keepalive();
-                        log::trace!("{} : keepalive queued", peer);
-                        timers
-                            .send_persistent_keepalive
-                            .start(Duration::from_secs(timers.keepalive_interval));
-                    }
-                })
-            },
+            retransmit_handshake: Self::create_retransmit_handshake_timer(
+                wireguard_device.clone(),
+                public_key_of_peer,
+                &runner,
+            ),
+            send_keepalive: Self::create_send_keepalive_timer(
+                wireguard_device.clone(),
+                public_key_of_peer,
+                &runner,
+            ),
+            new_handshake: Self::create_new_handshake_timer(
+                wireguard_device.clone(),
+                public_key_of_peer,
+                &runner,
+            ),
+            zero_key_material: Self::create_zero_key_material_timer(
+                wireguard_device.clone(),
+                public_key_of_peer,
+                &runner,
+            ),
+            send_persistent_keepalive: Self::create_send_persistent_keepalive_timer(
+                wireguard_device.clone(),
+                public_key_of_peer,
+                &runner,
+            ),
         }
+    }
+
+    fn create_retransmit_handshake_timer<T: Tun, B: UDP>(
+        wireguard_device: WireGuard<T, B>,
+        public_key_of_peer: PublicKey,
+        runner: &Runner,
+    ) -> Timer {
+        runner.timer(move || {
+            // fetch peer by public key
+            fetch_peer!(wireguard_device, public_key_of_peer, peer);
+            fetch_timers!(peer, timers);
+
+            // check if handshake attempts remaining
+            let attempts = timers.handshake_attempts.fetch_add(1, Ordering::SeqCst);
+            if attempts > MAX_TIMER_HANDSHAKES {
+                debug!(
+                    "Handshake for peer {} did not complete after {} attempts, giving up",
+                    peer,
+                    attempts + 1
+                );
+                timers.send_keepalive.stop();
+                timers.zero_key_material.start(REJECT_AFTER_TIME * 3);
+                peer.purge_staged_packets();
+            } else {
+                debug!(
+                    "Handshake for {} did not complete after {} seconds, retrying (try {})",
+                    peer,
+                    REKEY_TIMEOUT.as_secs(),
+                    attempts
+                );
+                timers.retransmit_handshake.reset(REKEY_TIMEOUT);
+                peer.clear_src();
+                peer.packet_send_queued_handshake_initiation(true);
+            }
+        })
+    }
+
+    fn create_send_keepalive_timer<T: Tun, B: UDP>(
+        wireguard_device: WireGuard<T, B>,
+        public_key_of_peer: PublicKey,
+        runner: &Runner,
+    ) -> Timer {
+        runner.timer(move || {
+            // fetch peer by public key
+            fetch_peer!(wireguard_device, public_key_of_peer, peer);
+            fetch_timers!(peer, timers);
+
+            // send keepalive and schedule next keepalive
+            peer.send_keepalive();
+            if timers.needs_another_keepalive() {
+                timers.send_keepalive.start(KEEPALIVE_TIMEOUT);
+            }
+        })
+    }
+
+    fn create_new_handshake_timer<T: Tun, B: UDP>(
+        wireguard_device: WireGuard<T, B>,
+        public_key_of_peer: PublicKey,
+        runner: &Runner,
+    ) -> Timer {
+        runner.timer(move || {
+            // fetch peer by public key
+            fetch_peer!(wireguard_device, public_key_of_peer, peer);
+            fetch_timers!(peer, timers);
+
+            // clear source and retry
+            log::debug!(
+                "Retrying handshake with {} because we stopped hearing back after {} seconds",
+                peer,
+                (KEEPALIVE_TIMEOUT + REKEY_TIMEOUT).as_secs()
+            );
+            peer.clear_src();
+            peer.packet_send_queued_handshake_initiation(false);
+        })
+    }
+
+    fn create_zero_key_material_timer<T: Tun, B: UDP>(
+        wireguard_device: WireGuard<T, B>,
+        public_key_of_peer: PublicKey,
+        runner: &Runner,
+    ) -> Timer {
+        runner.timer(move || {
+            // fetch peer by public key
+            fetch_peer!(wireguard_device, public_key_of_peer, peer);
+            log::trace!("{} : timer fired (zero_key_material)", peer);
+
+            // null all key-material
+            peer.zero_keys();
+        })
+    }
+
+    fn create_send_persistent_keepalive_timer<T: Tun, B: UDP>(
+        wireguard_device: WireGuard<T, B>,
+        public_key_of_peer: PublicKey,
+        runner: &Runner,
+    ) -> Timer {
+        runner.timer(move || {
+            // fetch peer by public key
+            fetch_peer!(wireguard_device, public_key_of_peer, peer);
+            fetch_timers!(peer, timers);
+            log::trace!("{} : timer fired (send_persistent_keepalive)", peer);
+
+            // send and schedule persistent keepalive
+            if timers.keepalive_interval > 0 {
+                timers.send_keepalive.stop();
+                peer.send_keepalive();
+                log::trace!("{} : keepalive queued", peer);
+                timers
+                    .send_persistent_keepalive
+                    .start(Duration::from_secs(timers.keepalive_interval));
+            }
+        })
     }
 
     pub fn get_keepalive_interval(&self) -> u64 {
