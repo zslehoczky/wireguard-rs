b2251e44636b6281f77ba6032b26adf565259ac5
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-11-04 21:55:44 +0100

    Add helper fn
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- wg-platform/src/std_lib/udp_owner.rs
+++ wg-platform/src/std_lib/udp_owner.rs
@@ -4,6 +4,14 @@ use std::sync::Arc;
 
 use wg_traits::udp::Owner;
 
+fn mask_random_port(requested_port: u16, assigned_socket: &UdpSocket) -> u16 {
+    if requested_port == 0 {
+        0
+    } else {
+        assigned_socket.local_addr().unwrap().port()
+    }
+}
+
 pub struct StdUdpOwner {
     _sockets: Vec<Arc<UdpSocket>>,
     port: u16,
@@ -11,22 +19,8 @@ pub struct StdUdpOwner {
 
 impl StdUdpOwner {
     pub fn new(socket_v4: Arc<UdpSocket>, socket_v6: Arc<UdpSocket>, port: u16) -> Self {
-        debug_assert_eq!(
-            if port == 0 {
-                0
-            } else {
-                socket_v4.local_addr().unwrap().port()
-            },
-            port
-        );
-        debug_assert_eq!(
-            if port == 0 {
-                0
-            } else {
-                socket_v6.local_addr().unwrap().port()
-            },
-            port
-        );
+        debug_assert_eq!(mask_random_port(port, &socket_v4), port);
+        debug_assert_eq!(mask_random_port(port, &socket_v6), port);
 
         Self {
             _sockets: vec![socket_v4, socket_v6],
