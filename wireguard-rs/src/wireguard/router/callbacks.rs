efa8578363f484d4611428bb4702e0b7a6d3c7d1
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-11-21 00:16:04 +0100

    Remove Opaque trait
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- wireguard-rs/src/wireguard/router/callbacks.rs
+++ wireguard-rs/src/wireguard/router/callbacks.rs
@@ -2,12 +2,8 @@ use alloc::sync::Arc;
 
 use crate::wireguard::peer::KeyPair;
 
-pub trait Opaque: Send + Sync + 'static {}
-
-impl<T> Opaque for T where T: Send + Sync + 'static {}
-
 pub trait Callbacks: Send + Sync + 'static {
-    type Opaque: Opaque;
+    type Opaque: Send + Sync + 'static;
     fn send(opaque: &Self::Opaque, size: usize, sent: bool, keypair: &Arc<KeyPair>, counter: u64);
     fn recv(opaque: &Self::Opaque, size: usize, sent: bool, keypair: &Arc<KeyPair>);
     fn need_key(opaque: &Self::Opaque);
