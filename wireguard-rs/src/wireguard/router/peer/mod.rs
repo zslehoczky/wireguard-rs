48f7efbda1f97247faf1957b44c143e53d664bd9
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-11-21 11:55:38 +0100

    Fix warning
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- wireguard-rs/src/wireguard/router/peer/mod.rs
+++ wireguard-rs/src/wireguard/router/peer/mod.rs
@@ -2,7 +2,7 @@ mod anti_replay;
 mod decryption_state;
 mod encryption_state;
 mod key_wheel;
-mod peer;
+mod peer_inner;
 
 use std::sync::Arc;
 
@@ -10,7 +10,7 @@ use crate::wireguard::peer::KeyPair;
 
 pub use decryption_state::DecryptionState;
 use encryption_state::EncryptionState;
-pub use peer::{Peer, PeerHandle, new_peer};
+pub use peer_inner::{Peer, PeerHandle, new_peer};
 
 fn crypto_state<P>(peer: P, keypair: Arc<KeyPair>) -> (EncryptionState, DecryptionState<P>) {
     (
