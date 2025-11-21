b838350560751f3a74e43400287b0cb14a24b9b1
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-11-21 13:00:42 +0100

    Rename inner peer module
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- wireguard-rs/src/wireguard/router/peer/mod.rs
+++ wireguard-rs/src/wireguard/router/peer/mod.rs
@@ -2,7 +2,8 @@ mod anti_replay;
 mod decryption_state;
 mod encryption_state;
 mod key_wheel;
-mod peer_inner;
+#[allow(clippy::module_inception)]
+mod peer;
 
 use std::sync::Arc;
 
@@ -10,7 +11,7 @@ use super::KeyPair;
 
 pub use decryption_state::DecryptionState;
 use encryption_state::EncryptionState;
-pub use peer_inner::{Peer, PeerHandle, new_peer};
+pub use peer::{Peer, PeerHandle, new_peer};
 
 fn crypto_state<P>(peer: P, keypair: Arc<KeyPair>) -> (EncryptionState, DecryptionState<P>) {
     (
