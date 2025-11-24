1d66079bf60680417921e6d82755d234cfc0a433
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-11-24 12:19:26 +0100

    Move module documentation
    Signed-off-by: Zsombor Lehoczky <zslehoczky@users.noreply.github.com>

--- wireguard-rs/src/lib.rs
+++ wireguard-rs/src/lib.rs
@@ -7,5 +7,15 @@ mod configuration;
 pub mod router;
 pub mod run;
 pub mod timers;
+
+/// The wireguard sub-module represents a full, pure, WireGuard implementation:
+///
+/// The WireGuard device described here does not depend on particular IO implementations
+/// or UAPI, and can be instantiated in unit-tests with the dummy IO implementation.
+///
+/// The code at this level serves to "glue" the handshake state-machine
+/// and the crypto-key router code together,
+/// e.g. every WireGuard peer consists of one handshake peer and one router peer.
 pub mod wireguard;
+
 pub mod workers;
--- wireguard-rs/src/wireguard/mod.rs
+++ wireguard-rs/src/wireguard/mod.rs
@@ -1,12 +1,3 @@
-/// The wireguard sub-module represents a full, pure, WireGuard implementation:
-///
-/// The WireGuard device described here does not depend on particular IO implementations
-/// or UAPI, and can be instantiated in unit-tests with the dummy IO implementation.
-///
-/// The code at this level serves to "glue" the handshake state-machine
-/// and the crypto-key router code together,
-/// e.g. every WireGuard peer consists of one handshake peer and one router peer.
-
 #[cfg(test)]
 pub mod tests;
 
